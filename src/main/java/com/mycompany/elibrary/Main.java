2ad52bbf26f7ea208992732c513b255d63ee63e7
aldor <aldor@LAPTOP-BCNKP567>
2025-06-22 11:15:09 +0700

    First Commit

--- src/main/java/com/mycompany/elibrary/Main.java
+++ src/main/java/com/mycompany/elibrary/Main.java
@@ -10,6 +10,6 @@ package com.mycompany.elibrary;
  */
 public class Main {
     public static void main(String[] args) {
-        new LoginFrame().setVisible(true);
+        new LoginFrame().setVisible(true); //
     }
 }
